89c04125a5e8bbe408675d27775ae36fe285bcff
abdullahjahangir <jahangirabdullah300@gmail.com>
2024-09-26 15:28:55 +0600

    b10a5 done

---
+++ js/qpp.js
@@ -0,0 +1,9 @@
+document.getElementById('button-donate').addEventListener('click',function(event){
+    event.preventDefault();
+    
+
+    // get amount
+    const writeAmount = document.getElementById('write-ammount').value;
+    console.log(writeAmount)
+})
+
